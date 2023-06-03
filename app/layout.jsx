7de7c8ce52e1ba2bbd01b9ce1847b2e1ed558e5c
Kino <andresprza@gmail.com>
2023-06-03 12:45:12 -0600

    setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
-import { ClerkProvider } from "@clerk/nextjs"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -11,10 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang='en'>
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
   )
 }
