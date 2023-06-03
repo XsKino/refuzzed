eee3de4fc04c753d1efdea7571e2a8e5dc8db953
SM-BD <mavin323lol@gmail.com>
2023-06-03 13:18:29 -0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,6 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center content-center bg-red-500'>sassinet</main>
+    <main className='flex min-h-screen flex-col items-center content-center bg-red-500'>SASMASPRO</main>
   )
 }
