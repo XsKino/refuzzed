9b82ac486296bd7c0ffbf79e559a134d7712e72a
Kino <andresprza@gmail.com>
2023-06-16 14:01:33 -0600

    Initial commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
 import Image from "next/image"
 
 export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center content-center bg-red-500'>KINO</main>
-  )
+  return <main>Buenas Tardes</main>
 }
