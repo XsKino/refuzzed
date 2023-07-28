94cc3985ce9dbc83f97a27b6c6da4a59767be4a7
Kino <andresprza@gmail.com>
2023-07-28 15:57:10 -0600

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "standard",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
+  extends: ["standard", "eslint:recommended", "plugin:react/recommended", "next", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "react-hooks"],
+  plugins: ["react"],
   rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
   },
 }
